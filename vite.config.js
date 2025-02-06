9a1dfad8d909d36635072afe4cd05add0c3e875f
Anirudh Kadian <coderanirudh@gmail.com>
2025-02-06 21:26:49 +0530

    deploy try3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/qodweather/",
+  base: "/qodweather/", // Ensure this matches your repo name
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
 });
